aa748aa671a0f44709d12cef7f554e06beab314f
刘志伟 <lzw@spacetech.com.cn>
2020-07-03 17:57:41 +0800

    pm2 配置

---
+++ pm2.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      // 指定解释器
+      interpreter:
+        '/Users/Eucalyptus_leave/.config/yarn/global/node_modules/.bin/ts-node',
+      // 解释器参数 -P 表示项目路径，会自动使用项目的 tsconfig.json
+      // interpreter_args: '-P ./src -r tsconfig-paths/register',
+      cwd: './',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      kill_timeout: 10000,
+      name: 'app',
+      script: './src/app.ts',
+      wait_ready: true,
+      ignore_watch: ['node_modules']
+    }
+  ]
+}
