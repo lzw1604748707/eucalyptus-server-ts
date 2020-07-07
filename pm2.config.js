fde171a138f46cdd97f942ddc409f7d926262177
刘志伟 <lzw@spacetech.com.cn>
2020-07-07 13:44:12 +0800

    更新

--- pm2.config.js
+++ pm2.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       // 指定解释器
-      interpreter: `${process.env.HOME ||
-        process.env.USERPROFILE}/.config/yarn/global/node_modules/.bin/ts-node`,
+      // interpreter: `${process.env.HOME ||
+      //   process.env.USERPROFILE}/.config/yarn/global/node_modules/.bin/ts-node`,
       // 解释器参数 -P 表示项目路径，会自动使用项目的 tsconfig.json
       // interpreter_args: '-P ./src -r tsconfig-paths/register',
       cwd: './',
