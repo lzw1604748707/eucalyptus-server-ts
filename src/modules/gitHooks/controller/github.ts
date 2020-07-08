8853955e6d9b7f017012cfecdecc493546988478
刘志伟 <lzw@spacetech.com.cn>
2020-07-08 11:49:27 +0800

    自动部署测试

--- src/modules/gitHooks/controller/github.ts
+++ src/modules/gitHooks/controller/github.ts
@@ -1,16 +1,17 @@
 import Router from 'koa-router'
 import {ParameterizedContext, Context} from 'koa'
-import {exec} from 'child_process'
 
 import githubSevice from '../service/github'
 const router = new Router()
 
 router.post('/push', async (ctx: ParameterizedContext | Context, text) => {
   const request = ctx.request
-
-  githubSevice.reAsignAutoDeploy(request)
-
-  ctx.body = new Date().toDateString() + '  ' + '更新部署'
+  try {
+    githubSevice.reAsignAutoDeploy(request)
+    ctx.body = new Date().toDateString() + '  ' + '更新部署'
+  } catch (error) {
+    await ctx.render('error', error)
+  }
 })
 
 export default router
--- src/modules/gitHooks/service/github.ts
+++ src/modules/gitHooks/service/github.ts
@@ -1,32 +1,43 @@
 import Application from 'koa'
-
+import {exec} from 'child_process'
+import crypto from 'crypto'
 class GithubSevice {
   /**
    * 本地执行自动部署
    */
   public reAsignAutoDeploy(request: Application.Request) {
-    exec('echo "' + JSON.stringify(request) + '">> ./test.log')
+    // this.verifySignature(request)
+    const {repository} = request.body
+    exec('chmod -R u+x ../"' + repository.name + '"/package.sh')
+    exec('../"' + repository.name + '"/package.sh')
   }
 
   /**
    * 验证签名信息
    */
-  public verifySignature(request: Application.Request) {
-    const signature = request.headers['x-hub-signature']
-    const event = request.headers['x-github-event']
-    const id = request.headers['x-github-delivery']
+  private verifySignature(request: Application.Request) {
+    const errorMessageMap: Record<string, any> = {
+      'x-hub-signature': '没有钩子签名哦~',
+      'x-github-event': '你还没说你要用什么方法呢！',
+      'x-github-delivery': '你也不给个身份吗！！'
+    }
 
-    // if (!signature) {
-    //   return hasError('No X-Hub-Signature found on request')
-    // }
+    for (const key in errorMessageMap) {
+      if (!request.headers[key]) throw Error(errorMessageMap[key])
+    }
 
-    // if (!event) {
-    //   return hasError('No X-Github-Event found on request')
-    // }
-
-    // if (!id) {
-    //   return hasError('No X-Github-Delivery found on request')
-    // }
+    const signature = Buffer.from(request.headers['x-hub-signature'])
+    const localSignature = Buffer.from(
+      `sha1=${crypto
+        .createHmac('sha1', 'enculyptus')
+        .update(signature)
+        .digest('hex')}`
+    )
+    if (
+      signature.length !== localSignature.length ||
+      !crypto.timingSafeEqual(signature, localSignature)
+    )
+      throw new Error('不能做坏事！')
   }
 }
 
